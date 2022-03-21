09a6e28d99aa7deda1a9e12cd89c859c686a3b7d
Sander Steffann <sander@steffann.nl>
2022-03-22 00:56:52 +0100

    Only show donors in API

--- contacts/api.py
+++ contacts/api.py
@@ -18,15 +18,15 @@ class OrganisationSerializer(HyperlinkedModelSerializer):
 
 
 # ViewSets define the view behavior.
-class OrganisationViewSet(ReadOnlyModelViewSet):
-    queryset = Organisation.objects.filter(listed=True)
+class DonorOrganisationViewSet(ReadOnlyModelViewSet):
+    queryset = Organisation.objects.filter(listed=True, contacts__groups__name__icontains='donor').distinct()
     serializer_class = OrganisationSerializer
     filterset_fields = ['name']
     search_fields = ['name']
 
 
-class ContactViewSet(ReadOnlyModelViewSet):
-    queryset = Contact.objects.filter(listed=True)
+class PersonalDonorViewSet(ReadOnlyModelViewSet):
+    queryset = Contact.objects.filter(listed=True, groups__name__icontains='donor')
     serializer_class = ContactSerializer
     filterset_fields = ['first_name', 'last_name']
     search_fields = ['fist_name', 'last_name']
