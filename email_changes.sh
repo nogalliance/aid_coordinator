62ba65ae995ef5b293b9d19f427751e38fe13be3
Sander Steffann <sander@steffann.nl>
2022-04-02 19:05:27 +0200

    Set absolute paths in cronjob script

--- email_changes.sh
+++ email_changes.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
+# Use gnaukraine user
+HOME=$(/usr/bin/realpath ~gnaukraine)
+export HOME
+
 YESTERDAY=$(date --date="yesterday" +"%Y-%m-%d")
 WHEN=${1:-$YESTERDAY}
 
-cd "$(dirname "$0")"
-poetry run ./manage.py show_changes "$WHEN" | mail \
+cd "$(/usr/bin/dirname "$0")"
+/usr/local/bin/poetry run ./manage.py show_changes "$WHEN" | /usr/bin/mail \
   -s "KeepUkraineConnected changes of $WHEN" \
   -a "From: GNA web server <noreply@nogalliance.org>" \
   ukraine@nogalliance.org
