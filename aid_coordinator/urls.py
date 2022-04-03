1fbd2567b1eec2f155e1c1a5cff5ba5eb75f0f6f
Sander Steffann <sander@steffann.nl>
2022-04-03 07:35:20 +0200

    Add some GNA styling

--- aid_coordinator/urls.py
+++ aid_coordinator/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.views import PasswordResetView
 from django.urls import path, include
 from rest_framework import routers
-
+from django.utils.translation import gettext_lazy as _
 from contacts.api import DonorOrganisationViewSet, PersonalDonorViewSet
 from supply_demand.api import OfferItemViewSet, RequestItemViewSet
+
+# Change titles
+admin.site.site_title = _('Keep Ukraine Connected')
+admin.site.site_header = _('GNA Keep Ukraine Connected back-end')
+admin.site.index_title = _("Donation administration")
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
